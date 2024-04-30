f155c3e0c16f83e3b93ffa7ba1de9f9208ed3c07
Strahinja Gligović <31292812+strahinja-gligovic@users.noreply.github.com>
2024-04-30 23:13:47 +0200

    test pipeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function HomePage() {
       <section className="py-16 md:py-24 lg:py-32">
         <div className="container flex max-w-screen-md flex-col items-center gap-4 text-center">
           <h1 className="!leading-none font-bold text-3xl sm:text-5xl md:text-6xl lg:text-7xl landing-header py-2">
-            Lisan <strong>Deoba</strong> strong
+            Lisan <strong>Deoba</strong>
           </h1>
           <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
             U carstvu Vile Kamenjarke, gde se hrabri momci rađaju kao legende,
